072edee30640813c0e07d5fcddfb30dfc4883f3a
Blap00 <fa.palma@duocuc.cl>
2023-06-08 19:27:27 -0400

    UPD LESS TRY and DOCUMENT ALGORITMOS IV

--- CSS/Alogritmos IV/Js/AlgoApp.js
+++ CSS/Alogritmos IV/Js/AlgoApp.js
@@ -21,13 +21,21 @@ console.log("EJERCICIO 2");
 function calcularEstadisticas(arr) {
     const maximum = Math.max(...arr); // Obtiene el valor máximo del array utilizando la función Math.max(). El operador spread (...) se utiliza para expandir los elementos del array como argumentos individuales.
     const minimum = Math.min(...arr); // Obtiene el valor mínimo del array utilizando la función Math.min(). Al igual que en el caso anterior, el operador spread (...) se utiliza para expandir los elementos del array.
+    /*
+     * const sum: Declara una constante llamada sum para almacenar el resultado de la suma.
+     * arr.reduce: Aplica el método reduce al array arr. El método reduce se utiliza para reducir los elementos de un array a un solo valor.
+     * (acc, num) => acc + num: Es una función de flecha (arrow function) que se utiliza como argumento para el método reduce. Toma dos parámetros: acc (acumulador) y num (el elemento actual del array). En cada iteración, esta función suma el valor del acumulador acc con el elemento num.
+     * , 0: Es el segundo argumento pasado al método reduce y se utiliza como valor inicial para el acumulador. En este caso, se establece en 0.
+     * El resultado final es el valor de la suma de todos los elementos del array arr, que se almacena en la constante sum.
+     * En resumen, la línea de código utiliza el método reduce para sumar todos los valores del array arr.
+     * La función de flecha especificada como argumento define la operación de suma, utilizando un acumulador que se inicializa en 0. El resultado se almacena en la constante sum.
+     */
     const sum = arr.reduce((acc, num) => acc + num, 0); // Utiliza el método reduce() para sumar todos los valores del array. El acumulador (acc) va almacenando la suma acumulada y se inicializa en 0. Se utiliza una función de flecha (arrow function) para definir la operación de suma.
-    const average = sum / arr.length; // Calcula el promedio dividiendo la suma total entre la longitud del array.
+    const average = sum / arr.length;
     console.log("Máximo:", maximum); // Imprime el valor máximo.
     console.log("Mínimo:", minimum); // Imprime el valor mínimo.
     console.log("Promedio:", average); // Imprime el promedio.
 }
-
 // Ejemplo de la funcion:
 const arr = [10, 5, 7, 3, 9];
 calcularEstadisticas(arr);
@@ -35,12 +43,19 @@ calcularEstadisticas(arr);
 console.log("EJERCICIO 3");
 //Dado un array de números, crea una función que dé como resultado un nuevo array donde los valores negativos se reemplacen por el texto (string) ‘Dojo’. Por ejemplo, reemplazarNegativos([1,2,-3,-5,5]) debiera devolver [1,2, “Dojo”, “Dojo”, 5].
 function reemplazarNegativos(arr1) {
+    /**
+     * MAP: para iterar a través de los elementos dentro de una colección de arreglos en JavaScript.
+     * 
+     * const newArray: Declara una nueva constante llamada newArray para almacenar el resultado de la operación map.
+     * arr1.map: Aplica el método map al array arr1. El método map se utiliza para crear un nuevo array con los resultados de aplicar una función a cada elemento del array original.
+     * (num) => (num < 0 ? "Dojo" : num): Es una función de flecha (arrow function) que se utiliza como argumento para el método map. Toma un parámetro num, que representa cada elemento del array arr1. En cada iteración, esta función verifica si el número num es menor que 0. Si es así, devuelve la cadena de texto "Dojo". Si no, devuelve el número num sin modificar.
+     * El resultado final es un nuevo array llamado newArray, donde cada elemento es el resultado de aplicar la función definida en el paso anterior a los elementos correspondientes del array arr1.
+     */
     // Utiliza el método map() para crear un nuevo array basado en el array original
     const newArray = arr1.map((num) => (num < 0 ? "Dojo" : num));
     // Si el número es negativo, se reemplaza por el string "Dojo", de lo contrario se mantiene el número original
     return newArray; // Retorna el nuevo array
 }
-
 // Ejemplo de esta funcion:
 const arr1 = [1, 2, -3, -5, 5];
 const newArray = reemplazarNegativos(arr1); // Llama a la función reemplazarNegativos y pasa el array como argumento
