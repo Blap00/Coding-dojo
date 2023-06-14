0399a01e03ee99ceb1c99738599c9f8b5a879290
Blap00 <fa.palma@duocuc.cl>
2023-06-13 21:15:38 -0400

    Upd Fundamentos Basicos I

--- Js/FUNCION I/js/archivo.js
+++ Js/FUNCION I/js/archivo.js
@@ -2,40 +2,40 @@
 function generarArregloAleatorio() {
     var arregloAleatorio = [];
     for (var i = 0; i < 10; i++) {
-      var numeroAleatorio = Math.floor(Math.random() * 20) + 1;
-      arregloAleatorio.push(numeroAleatorio);
+        var numeroAleatorio = Math.floor(Math.random() * 20) + 1;
+        arregloAleatorio.push(numeroAleatorio);
     }
     return arregloAleatorio;
-  }
-  
-  // Función para comparar dos arreglos y contar las coincidencias
-  function compararArreglos(arr1, arr2) {
+}
+
+// Función para comparar dos arreglos y contar las coincidencias
+function compararArreglos(arr1, arr2) {
     var coincidencias = 0;
     for (var i = 0; i < 10; i++) {
-      if (arr1[i] === arr2[i]) {
-        coincidencias++;
-      }
+        if (arr1[i] === arr2[i]) {
+            coincidencias++;
+        }
     }
     return coincidencias;
-  }
-  
-  // Función principal para comparar los números
-  function compararNumeros(numeros) {
+}
+
+// Función principal para comparar los números
+function compararNumeros(numeros) {
     // Generar un arreglo aleatorio
     var arregloAleatorio = generarArregloAleatorio();
-  
+
     // Mostrar ambos arreglos por consola
     console.log("Arreglo ingresado:", numeros);
     console.log("Arreglo aleatorio:", arregloAleatorio);
-  
+
     // Contar las coincidencias entre los arreglos
     var cantidadCoincidencias = compararArreglos(numeros, arregloAleatorio);
-  
+
     // Mostrar la cantidad de coincidencias por consola
     console.log("Coincidencias encontradas:", cantidadCoincidencias);
-  }
-  
-  // Ejemplo de uso de la función compararNumeros con un arreglo de 10 números
-  var numeros = [3, 8, 15, 7, 12, 5, 18, 10, 1, 6];
-  compararNumeros(numeros);
+}
+
+// Ejemplo de uso de la función compararNumeros con un arreglo de 10 números
+var numeros = [4, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+compararNumeros(numeros);
 
