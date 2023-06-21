adca12333150655934a284b4833f84eee951ee10
Blap00 <fa.palma@duocuc.cl>
2023-06-20 21:11:35 -0400

    Fix Blogs.vistapaginas

--- MySQL/Blogs/blogs.sql
+++ MySQL/Blogs/blogs.sql
@@ -117,6 +117,58 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Comentario` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `mydb`.`Archivos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Archivos` (
+  `idArchivos` INT NOT NULL,
+  `Administradores_idAdmin` INT NOT NULL,
+  `Publicaciones_idPublicaciones` INT NOT NULL,
+  `creadoEn` TIMESTAMP NULL,
+  `actualizadoEn` TIMESTAMP NULL,
+  `pathArchivo` VARCHAR(45) NULL,
+  PRIMARY KEY (`idArchivos`),
+  INDEX `fk_Archivos_Administradores1_idx` (`Administradores_idAdmin` ASC) VISIBLE,
+  INDEX `fk_Archivos_Publicaciones1_idx` (`Publicaciones_idPublicaciones` ASC) VISIBLE,
+  CONSTRAINT `fk_Archivos_Administradores1`
+    FOREIGN KEY (`Administradores_idAdmin`)
+    REFERENCES `mydb`.`Administradores` (`idAdmin`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Archivos_Publicaciones1`
+    FOREIGN KEY (`Publicaciones_idPublicaciones`)
+    REFERENCES `mydb`.`Publicaciones` (`idPublicaciones`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`VistadePaginas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`VistadePaginas` (
+  `idVistadePaginas` INT NOT NULL,
+  `user_idUsuario` INT NOT NULL,
+  `Blog_idBlog` INT NOT NULL,
+  `direccionIP` VARCHAR(20) NULL,
+  `TiempoVisita` VARCHAR(45) NULL,
+  `ultimaVisita` TIMESTAMP NULL,
+  PRIMARY KEY (`idVistadePaginas`),
+  INDEX `fk_VistadePaginas_user1_idx` (`user_idUsuario` ASC) VISIBLE,
+  INDEX `fk_VistadePaginas_Blog1_idx` (`Blog_idBlog` ASC) VISIBLE,
+  CONSTRAINT `fk_VistadePaginas_user1`
+    FOREIGN KEY (`user_idUsuario`)
+    REFERENCES `mydb`.`user` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_VistadePaginas_Blog1`
+    FOREIGN KEY (`Blog_idBlog`)
+    REFERENCES `mydb`.`Blog` (`idBlog`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
