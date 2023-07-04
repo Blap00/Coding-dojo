b051b2d2708d2100708f2f0ee6a4262ebbe90c4a
Blap00 <fa.palma@duocuc.cl>
2023-07-04 19:51:44 -0400

    Fix Herency with assigment

--- JAVA/work/javaOOP/src/com/CodingDojo/GuardiaZoologicoP2/BatTest.java
+++ JAVA/work/javaOOP/src/com/CodingDojo/GuardiaZoologicoP2/BatTest.java
@@ -21,3 +21,27 @@ public class BatTest {
         System.out.println("Nivel de energía final: " + bat.getEnergy());
     }
 }
+/*
+//NUEVA VERSION
+public class BatTest {
+   public static void main(String[] args) {
+       Bat bat = new Bat();
+
+       // Atacar la ciudad tres veces
+       bat.attackTown();
+       bat.attackTown();
+       bat.attackTown();
+
+       // Comer dos humanos
+       bat.eatHumans();
+       bat.eatHumans();
+
+       // Volar dos veces
+       bat.fly();
+       bat.fly();
+
+       // Mostrar nivel de energía final
+       System.out.println("Nivel de energía final: " + bat.getEnergyLevel());
+   }
+}
+*/
