9725b063ac06d6e3a19c78674a5a7e6880a1e8b6
Blap00 <fa.palma@duocuc.cl>
2023-07-10 23:09:27 -0400

    Fix Guesscontroller

--- JAVA/work/javaOnTheWeb/src/com/codingDojo/GreatNumberGame/src/main/java/com/fabianpalma/web/controllers/GuessController.java
+++ JAVA/work/javaOnTheWeb/src/com/codingDojo/GreatNumberGame/src/main/java/com/fabianpalma/web/controllers/GuessController.java
@@ -1,46 +1,53 @@
 package com.fabianpalma.web.controllers;
 
 import java.io.IOException;
+import java.util.Random;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import com.fabianpalma.web.models.GuessModel;
 
 @WebServlet("/guess")
 public class GuessController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        session.invalidate(); // Reiniciar sesión en cada nuevo juego
-        request.getRequestDispatcher("/WEB-INF/resultado.jsp").forward(request, response);
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate(); // Reiniciar sesión en cada nuevo juego
+		request.getRequestDispatcher("/WEB-INF/resultado.jsp").forward(request, response);
+	}
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        GuessModel model = (GuessModel) session.getAttribute("model");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Integer numberToGuess = (Integer) session.getAttribute("numberToGuess");
+		if (numberToGuess == null) {
+			// Generar un nuevo número para adivinar
+			Random random = new Random();
+			numberToGuess = random.nextInt(100) + 1;
+			session.setAttribute("numberToGuess", numberToGuess);
+		}
 
-        if (model == null) {
-            model = new GuessModel();
-            session.setAttribute("model", model);
-            model.generateNumber(); // Generar un nuevo número para adivinar
-        }
+		String guessString = request.getParameter("guess");
+		int guess = 0;
+		if (guessString != null && !guessString.isEmpty()) {
+			guess = Integer.parseInt(guessString);
+		}
 
-        String guessString = request.getParameter("guess");
-        int guess = 0;
-        if (guessString != null && !guessString.isEmpty()) {
-            guess = Integer.parseInt(guessString);
-        }
+		String message;
+		if (guess == numberToGuess) {
+			message = "¡Felicidades! Adivinaste el número.";
+			session.removeAttribute("numberToGuess");
+		} else if (guess < numberToGuess) {
+			message = "Demasiado bajo. Intenta nuevamente.";// NUMERO: "+numberToGuess;
+		} else {
+			message = "Demasiado alto. Intenta nuevamente.";// NUMERO: "+numberToGuess;
+		}
 
-        model.setUserGuess(guess);
-        model.checkGuess();
-
-        request.getRequestDispatcher("/WEB-INF/resultado.jsp").forward(request, response);
-    }
+		session.setAttribute("message", message);
+		request.getRequestDispatcher("/WEB-INF/resultado.jsp").forward(request, response);
+	}
 
 }
